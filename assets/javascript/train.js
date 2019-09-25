71b8bc672b35433d2edbccf566cc0b992a5a150c
usmanmalik <usmandotmalik@gmail.com>
2019-09-24 21:23:39 -0400

    updating js,html and css

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,1 +1,34 @@
+$(document).ready(function(){
+
+    var config = {
+        apiKey: "AIzaSyAE5dWmau3ZxWPdTb49sGvDSqR2VCSOM68",
+        authDomain: "train-scheduler-4106f.firebaseapp.com",
+        databaseURL: "https://train-scheduler-4106f.firebaseio.com",
+        projectId: "train-scheduler-4106f",
+        storageBucket: "train-scheduler-4106f.appspot.com",
+        messagingSenderId: "376387335459"
+      };
+      firebase.initializeApp(config);
+      var database = firebase.database();
     
+      $("#submit").on("click", function() {
+              //VALUES FOR EACH VARIABLE IN HTML
+              var name = $('#nameInput').val().trim();
+              var dest = $('#destInput').val().trim();
+              var time = $('#timeInput').val().trim();
+              var freq = $('#freqInput').val().trim();
+          
+          // PUSH NEW ENTRY TO FIREBASE
+              database.ref().push({
+                  name: name,
+                  dest: dest,
+                  time: time,
+                  freq: freq,
+                  timeAdded: firebase.database.ServerValue.TIMESTAMP
+              });
+              // NO REFRESH
+              $("input").val('');
+              return false;
+          });
+          
+      });     
