23b6c133de830f6eb2f140b5a407706db8565240
usmanmalik <usmandotmalik@gmail.com>
2019-10-16 17:17:54 -0400

    again

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,34 +1,108 @@
-$(document).ready(function(){
+setInterval(updateClock, 1000);
 
-    var config = {
-        apiKey: "AIzaSyAE5dWmau3ZxWPdTb49sGvDSqR2VCSOM68",
-        authDomain: "train-scheduler-4106f.firebaseapp.com",
-        databaseURL: "https://train-scheduler-4106f.firebaseio.com",
-        projectId: "train-scheduler-4106f",
-        storageBucket: "train-scheduler-4106f.appspot.com",
-        messagingSenderId: "376387335459"
-      };
-      firebase.initializeApp(config);
-      var database = firebase.database();
-    
-      $("#submit").on("click", function() {
-              //VALUES FOR EACH VARIABLE IN HTML
-              var name = $('#nameInput').val().trim();
-              var dest = $('#destInput').val().trim();
-              var time = $('#timeInput').val().trim();
-              var freq = $('#freqInput').val().trim();
-          
-          // PUSH NEW ENTRY TO FIREBASE
-              database.ref().push({
-                  name: name,
-                  dest: dest,
-                  time: time,
-                  freq: freq,
-                  timeAdded: firebase.database.ServerValue.TIMESTAMP
-              });
-              // NO REFRESH
-              $("input").val('');
-              return false;
-          });
-          
-      });     
+// 1. Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyCsqUxxGxEf9B4LFsKMQP2PHNdGCQoNIzg",
+    authDomain: "malik-s-firebase.firebaseapp.com",
+    databaseURL: "https://malik-s-firebase.firebaseio.com",
+    projectId: "malik-s-firebase",
+    storageBucket: "malik-s-firebase.appspot.com",
+    messagingSenderId: "623189607790",
+    appId: "1:623189607790:web:774884c36b21659b54ed7f"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+function updateClock() {
+  var clock = moment().format("h:mm:ss a");
+
+  var c = $("<h2>");
+  var c2 = c.append(clock);
+  $("#clock").html(c2);
+}
+
+setInterval(updateClock, 1000);
+
+$("#add-train-btn").click(function(event) {
+  event.preventDefault();
+  var newTrain = $("#train-name-input")
+    .val()
+    .trim();
+  var newDestination = $("#destination-input")
+    .val()
+    .trim();
+  var newFirstTrain = $("#first-train-input")
+    .val()
+    .trim();
+  var newFrequency = $("#frequency-input")
+    .val()
+    .trim();
+
+  newObject = {
+    train: newTrain,
+    destination: newDestination,
+    firstTrain: newFirstTrain,
+    frequency: newFrequency
+  };
+
+  database.ref().push(newObject);
+
+  console.log(newObject.train);
+  console.log(newObject.destination);
+  console.log(newObject.firstTrain);
+  console.log(newObject.frequency);
+
+  alert("Train successfully added!");
+
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency-input").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val());
+
+  var newTrain = childSnapshot.val().train;
+  var newDestination = childSnapshot.val().destination;
+  var newFirstTrain = childSnapshot.val().firstTrain;
+  var newFrequency = childSnapshot.val().frequency;
+
+  console.log(newTrain);
+  console.log(newDestination);
+  console.log("FIRST TRAIN DEPARTED AT: " + newFirstTrain);
+  console.log("THE TRAIN ARRIVES EVERY " + newFrequency + " MINUTES");
+
+  var firstTrainConverted = moment(newFirstTrain, "hh:mm").subtract(1, "days");
+  console.log(firstTrainConverted);
+
+  var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  var timeApart = diffTime % newFrequency;
+  console.log("MINUTES TO SUBTRACT FROM FREQUENCY: " + timeApart);
+
+  var minutesAway = newFrequency - timeApart;
+  console.log("MINUTES UNTIL TRAIN: " + minutesAway);
+
+  var nextArrival = moment().add(minutesAway, "minutes");
+  var nextArrival2 = moment(nextArrival).format("hh:mm");
+
+  console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
+
+  $("#train-table > tbody").append(
+    "<tr><td>" +
+      newTrain +
+      "</td> <td>" +
+      newDestination +
+      "</td> <td>" +
+      newFrequency +
+      "</td><td>" +
+      nextArrival2 +
+      "</td><td>" +
+      minutesAway +
+      "</td></tr>"
+  );
+});
